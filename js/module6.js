f3c40fcff9f3c844209d7cb350c967d8ef6aff29
Vasyl Kondruk <vkondruk16@gmail.com>
2021-05-02 09:44:34 +0300

    ex 1-2

--- js/module6.js
+++ js/module6.js
@@ -1,1 +1,37 @@
-console.log('hello');
+// =================== ex. 1 =====================
+// Функция calculateTotalPrice(orderedItems) принимает один параметр orderedItems - массив чисел, и рассчитывает общую сумму его элементов, которая сохраняется в переменной totalPrice и возвращается как результат работы функции.
+
+// Выполни рефакторинг функции так, чтобы вместо цикла for она использовала метод forEach.
+
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+//   // Пиши код ниже этой строки
+
+//   orderedItems.forEach(function (orderedItem, index) {
+//     // console.log(orderedItem);
+//     totalPrice += orderedItem;
+//   });
+
+//   // Пиши код выше этой строки
+//   return totalPrice;
+// }
+
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+
+// =================== ex. 2 =====================
+// Функция filterArray(numbers, value) принимает массив чисел numbers и возвращает новый массив, в котором будут только те элементы оригинального массива, которые больше чем значение параметра value.
+
+// Выполни рефакторинг функции так, чтобы вместо цикла for она использовала метод forEach.
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+  // Пиши код ниже этой строки
+
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] > value) {
+      filteredNumbers.push(numbers[i]);
+    }
+  }
+
+  // Пиши код выше этой строки
+  return filteredNumbers;
+}
